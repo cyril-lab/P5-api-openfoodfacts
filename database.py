a4903c36d94b21543ba44db95e5a27eccbe800ab
Cyril Vernhes <c.vernhes3@gmail.com>
2020-06-25 15:34:48 +0200

    add menu application

--- database.py
+++ database.py
@@ -15,22 +15,16 @@ class Database:
                                                 database=database)
 
     def create_database(self, sql_script):
-
         cursor = self.mysql_connection.cursor()
-
         with open(sql_script, "r") as sql_script:
-         
             read_file = sql_script.read()
             sql_commands = read_file.split(';')
-
             for command in sql_commands:
                 try:
                     if command.strip() != '':
                         cursor.execute(command)
                 except IOError as exception:
                     print("Command skipped: ", exception)
-
-        #executeScriptsFromFile(sql_script)
         self.mysql_connection.commit()
 
 
@@ -40,35 +34,42 @@ class Database:
             cursor.execute(f"""INSERT INTO category (name) VALUES ("{element}")""")
         self.mysql_connection.commit()
         
-
-
-    def saved_product(self, liste_product, number_catgory):
+    def saved_product(self, liste_product, number_category):
         product_list = sorted(liste_product, key=lambda colonnes: colonnes[3])
         cursor = self.mysql_connection.cursor()
         for element in product_list:
             cursor.execute(f"""
             INSERT INTO product (name, store, url, nutriscore_grade, Category_id)
-                VALUES("{element[0]}", "{element[1]}", "{element[2]}", "{element[3]}", "{number_catgory}"
+                VALUES("{element[0]}", "{element[1]}", "{element[2]}", "{element[3]}", "{number_category}"
             );
             """)
         self.mysql_connection.commit()
         #self.mysql_connection.close()
 
+    def saved_product_substitution(self, id_product, id_product_substitution):
+        cursor = self.mysql_connection.cursor()
+        cursor.execute(f"""
+        INSERT INTO substitution (Product_id, Product_sub)
+            VALUES("{id_product}", "{id_product_substitution}"
+        );
+        """)
+        self.mysql_connection.commit()    
 
+    def get_list_product(self, category_number):
+        cursor = self.mysql_connection.cursor()
+        cursor.execute(f"""SELECT * FROM product WHERE Category_id = {category_number} ORDER BY id""")
+        tup = cursor.fetchall()
+        return tup
 
-category_pizzas = ClearData("fromages")
-category_pizzas.get_number_products()
-category_pizzas.calculate_number_page()
-category_pizzas.get_data_api()
-category_pizzas.generate_products_list()
+    def get_substitution(self):
+        cursor = self.mysql_connection.cursor()
+        cursor.execute(f"""SELECT * FROM substitution""")
+        tup = cursor.fetchall()
+        return tup
 
-
-mysql = Database("localhost", "cyril", "qsdf678/", "p5" )
-mysql.create_database("p5.sql")
-mysql.saved_category()
-
-mysql.saved_product(category_pizzas.products, 1)
-
-
-#print(category_pizzas.products)
+    def get_product(self, id_product):
+        cursor = self.mysql_connection.cursor()
+        cursor.execute(f"""SELECT *  FROM product WHERE id = {id_product}""")
+        tup = cursor.fetchall()
+        return tup
 
