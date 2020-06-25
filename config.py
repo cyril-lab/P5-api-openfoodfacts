66713835d7d06c69de67b3f6f5369ab7949666eb
Cyril Vernhes <c.vernhes3@gmail.com>
2020-06-25 15:37:40 +0200

    add menu

--- config.py
+++ config.py
@@ -2,10 +2,13 @@
 # -*- coding: Utf-8 -*
 
 #number of products return by one request (20, 50, 100, 250, 500 or 1000)
-NUMBER_PRODUCT = 100
+PRODUCT_PAGE = 100
 
 #number maxof products return by one request
-NUMBER_PRODUCT_MAX = 100
+NUMBER_PRODUCT_MAX = 180
 
 #categories
-CATEGORIES = ["Viandes", "Boissons", "Fromages", "Sauces"]
+CATEGORIES = ["Snacks", "Boissons", "Epicerie", "Sauces", "Desserts"]
+
+#number of products per page
+NUMBER_PRODUCT_DISPLAY = 15
--- requestapi.py
+++ requestapi.py
@@ -18,31 +18,35 @@ class ResquestApi:
     def __init__(self, category):
         self.number_products_max = config.NUMBER_PRODUCT_MAX
         self.category = category
-        self.number_products = 0
+        self.number_products = self._get_number_products()
         self.number_page = 1
-        self.results_page = config.NUMBER_PRODUCT
+        self.results_page = config.PRODUCT_PAGE
         self.data_category_json = []
 
-    def calculate_number_page(self):
-        """method to calculate the number of pages required for the request"""
-        if self.number_products > self.results_page:
-            self.number_page = (math.ceil(min(
-                
-                self.number_products,
-                self.number_products_max) / self.results_page))
-
-    def get_number_products(self):
+    def _get_number_products(self):
         """method to get the number of products in the category"""
         number_product_category = requests.get(
             f"https://fr.openfoodfacts.org/categorie/{self.category}.json")
         number_product_category_json = number_product_category.json()
-        self.number_products = int(number_product_category_json["count"])
+        #self.number_products = int(number_product_category_json["count"])
+        return int(number_product_category_json["count"])
+
+    def _calculate_number_page(self):
+        """method to calculate the number of pages required for the request"""
+        if self.number_products > self.results_page:
+            self.number_page = (math.ceil(min(
+                self.number_products,
+                self.number_products_max) / self.results_page))
+            return self.number_page
 
     def get_data_api(self):
         """method to get data produts"""
-        number_page = self.number_page
+        number_page = self._calculate_number_page()
 
         while number_page:
+            print(f"Téléchargement de {self.results_page} produits {self.category} "
+                  f"(requête {(self.number_page-number_page+1)}/{self.number_page}) ...")
+
             data_category = requests.get(
                 f"https://fr.openfoodfacts.org/cgi/search.pl?action=process"
                 f"&tagtype_0=categories&tag_contains_0=contains&"
